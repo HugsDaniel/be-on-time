46077213edc666c1811a6dcd9cb199382e72c530
Paula Lepore <paula_mpl@hotmail.com>
2021-11-26 17:06:18 +0100

    Done

--- app/javascript/components/menu.js
+++ app/javascript/components/menu.js
@@ -1,5 +1,5 @@
 const toggleMenu = () => {
-    const menu = document.querySelector(".menu")
+    const menu = document.querySelector(".menu-main")
     if (menu != null) {
         const button = document.querySelector("#pp-btn")
         button.addEventListener("click", (event) => {
