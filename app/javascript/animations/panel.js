4dd405ea0440b103e14700981180595e076e91dc
hugo roger <hugo.roger@pm.me>
2021-11-26 12:40:59 +0100

    add conditional to JS anim

--- app/javascript/animations/panel.js
+++ app/javascript/animations/panel.js
@@ -3,7 +3,7 @@ const panelTrigger = () => {
   const collapseBtn = document.querySelector('.btn-expand');
   const itineraryInfoPanel = document.querySelector('.intinerary-infos');
 
-
+  if (collapseBtn != null)
   collapseBtn.addEventListener('click', (e) => {
     collapseBtn.classList.toggle('btn-rotate');
 
