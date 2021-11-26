69b4a857ea85f4e5e991886a578c106fc1a5897e
hugo roger <hugo.roger@pm.me>
2021-11-26 16:40:08 +0100

    Fix bugs

--- db/migrate/20211124140653_remove_latitude_end_from_itineraries.rb
+++
@@ -1,5 +0,0 @@
-class RemoveLatitudeEndFromItineraries < ActiveRecord::Migration[6.0]
-  def change
-    remove_column :itineraries, :latitude_end, :float
-  end
-end
--- db/migrate/20211124140744_remove_longitude_end_from_itineraries.rb
+++
@@ -1,5 +0,0 @@
-class RemoveLongitudeEndFromItineraries < ActiveRecord::Migration[6.0]
-  def change
-    remove_column :itineraries, :longitude_end, :float
-  end
-end
